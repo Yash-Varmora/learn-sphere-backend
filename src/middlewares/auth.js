7c54ebe81f856a5ed2f75b8405f4df934a0936dc
Yash-Varmora <yash.varmora@bacancy.com>
2025-05-05 12:17:33 +0000

    [FIX] :  unhandled error

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,44 +4,50 @@ import userService from "../services/user.service.js"
 
 const authMiddleware = async (req, res, next) => {
     try {
-        const { access_token: accessToken, refresh_token: refreshToken } = req.cookies
+        const { access_token: accessToken, refresh_token: refreshToken } = req.cookies;
 
         if (!accessToken && !refreshToken) {
-            throw new CustomError(httpStatusCodes["Unauthorized"], "Invalid Access")
+            return next(new CustomError(httpStatusCodes["Unauthorized"], "Invalid Access"));
         }
 
         try {
-            const decodedData = token.verifyAccessToken(accessToken)
-            const userData = await userService.fetchUser({ email: decodedData.data.email })
-            const { password, ...user } = userData
-            if (!user) {
-                throw new CustomError(httpStatusCodes["Unauthorized"], "Invalid Access")
+            const decodedData = token.verifyAccessToken(accessToken);
+            const userData = await userService.fetchUser({ email: decodedData.data.email });
+
+            if (!userData) {
+                return next(new CustomError(httpStatusCodes["Unauthorized"], "Invalid Access"));
             }
-            await token.verifyRefreshToken(refreshToken, { user })
-            req.user = user
-            return next()
+
+            const { password, ...user } = userData;
+            await token.verifyRefreshToken(refreshToken, { user });
+            req.user = user;
+            return next();
         } catch (accessError) {
             if (!refreshToken) {
-                throw new CustomError(httpStatusCodes["Unauthorized"], "Invalid Access")
+                return next(new CustomError(httpStatusCodes["Unauthorized"], "Invalid Access"));
             }
+
             try {
-                const decodedData = token.verifyAccessToken(refreshToken)
-                const userData = await userService.fetchUser({ email: decodedData.data.email })
-                const { password, ...user } = userData
-                if (!user) {
-                    throw new CustomError(httpStatusCodes["Unauthorized"], "Invalid Access")
+                const decodedData = token.verifyAccessToken(refreshToken);
+                const userData = await userService.fetchUser({ email: decodedData.data.email });
+
+                if (!userData) {
+                    return next(new CustomError(httpStatusCodes["Unauthorized"], "Invalid Access"));
                 }
-                await token.verifyRefreshToken(refreshToken, { user })
-                req.user = user
-                return next()       
-            } catch (error) {
-                throw new CustomError(httpStatusCodes["Unauthorized"], "Invalid Access")
+
+                const { password, ...user } = userData;
+                await token.verifyRefreshToken(refreshToken, { user });
+                req.user = user;
+                return next();
+            } catch (refreshError) {
+                return next(new CustomError(httpStatusCodes["Unauthorized"], "Invalid Access"));
             }
         }
     } catch (error) {
-        console.log("====> Error authMiddleware", error.message)
-        throw new CustomError(httpStatusCodes["Unauthorized"], error.message)
+        console.log("====> Error authMiddleware", error.message);
+        return next(new CustomError(httpStatusCodes["Unauthorized"], error.message));
     }
-}
+};
+
 
 export default authMiddleware;
